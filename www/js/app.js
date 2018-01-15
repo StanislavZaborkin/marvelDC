7a3fdcd9c2e4339af9ccba04f240c9f1cefdeee9
Zyside <o.sirivlya@gmail.com>
2018-01-15 15:51:01 +0200

    console log for Marvel movies heros

--- www/js/app.js
+++ www/js/app.js
@@ -25,11 +25,42 @@ angular.module('starter', ['ionic'])
   };*/
 
   $scope.data1 = [];
-  $scope.click = false;
+  $scope.clickM = false;
+  $scope.clickD = false;
   $scope.click1 = false;
 
-  $scope.showMovies = function () {
+  $scope.getMarvelData = function () {
+    $scope.clickD = false;
+    $scope.clickM = true;
+    $scope.clone1 = [];
+    $scope.filteredArr = [];
+      for (var key in $scope.data1) {
+        $scope.clone1 = $scope.data1[key].marvel;
+      }
+      /*for (var i = 0; i < $scope.filteredArr.length; i++){
+        console.log($scope.filteredArr[i]);
+      }*/
+
+    /*for (keyy in $scope.filteredArr) {
+        if ($scope.filteredArr[keyy] === 'Spider-man') {
+          console.log($scope.filteredArr);
+        }
+
+      }*/
+    /*$scope.cast = [];
+    for (var i = 0; i < $scope.data1.length; i++) {
+      $scope.cast = $scope.data1[i];
+    }*/
+  };
+
+  $scope.showMarvelCartoons = function () {
     $scope.click1 = true;
+  };
+
+  $scope.showMarvelMovies = function () {
+    $scope.click1 = true;
+
+
   };
 
   $http({
@@ -41,31 +72,24 @@ angular.module('starter', ['ionic'])
     console.log('error' + response.statusText);
   });
 
-
-  $scope.getMarvelData = function () {
-    $scope.click = true;
-      $scope.clone1 = [];
-      for (var key in $scope.data1) {
-        $scope.clone1 = $scope.data1[key].marvel;
-      }
-      /*$scope.cast = [];
-      for (var i = 0; i < $scope.data1.length; i++) {
-        $scope.cast = $scope.data1[i];
-      }*/
-  };
-
-  $scope.getMarvelCartoons = function () {
-
-  };
-
   $scope.getDCData = function () {
-    $scope.click = true;
+    $scope.clickM = false;
+    $scope.clickD = true;
       for (var key in $scope.data1) {
         $scope.clone1 = $scope.data1[key].dc;
       }
+    console.log($scope.clone1);
   };
 
-  $scope.someFunction = function (h) {
-    console.log(h);
+  $scope.getMarvelHero = function (hero) {
+    console.clear();
+
+    for (key in $scope.clone1.movies) {
+      $scope.filteredArr.push($scope.clone1.movies[key].cast);
+      if (~$scope.clone1.movies[key].cast.indexOf(hero)){
+        console.log($scope.clone1.movies[key]);
+      }
+
+    }
   }
 });
